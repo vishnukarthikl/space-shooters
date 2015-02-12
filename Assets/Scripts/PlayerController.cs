508d8e9c23eaa7a7578e62854f5cb4f3315c51d2
Vishnu karthik <vishnucarbon@gmail.com>
2015-02-12 19:27:04 +0530

    Adding movement to player and removing Library

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class Boundary {
+	public float xMin,xMax,zMin,zMax;
+}
+
+public class PlayerController : MonoBehaviour {
+
+	public Boundary boundary;
+	public float speed;
+	public float tilt;
+
+	void FixedUpdate () {
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		rigidbody.velocity = movement * speed;
+
+		rigidbody.position = new Vector3 (
+			Mathf.Clamp(rigidbody.position.x,boundary.xMin,boundary.xMax),
+			0.0f,
+			Mathf.Clamp(rigidbody.position.z,boundary.zMin,boundary.zMax)
+			);
+
+		rigidbody.rotation = Quaternion.Euler (rigidbody.velocity.z * tilt, 0.0f, rigidbody.velocity.x * -tilt);
+
+	}
+}
