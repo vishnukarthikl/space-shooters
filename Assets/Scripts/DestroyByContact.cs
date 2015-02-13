e5ddcb85b1199d193d8bdf8f3e0b40ae3316fa43
Vishnu karthik <vishnucarbon@gmail.com>
2015-02-13 11:14:27 +0530

    Adding asteroid

---
+++ Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyByContact : MonoBehaviour {
+
+	public GameObject explosion;
+	public GameObject playerExplosion;
+
+	void OnTriggerEnter(Collider other) {
+		if (other.tag == "Boundary") {
+			return;
+		}
+		if (other.tag == "Player") {
+			Instantiate (playerExplosion, other.transform.position, other.transform.rotation);
+		}
+		Instantiate (explosion, transform.position, transform.rotation);
+		Destroy (other.gameObject);
+		Destroy (gameObject);
+	}
+}
